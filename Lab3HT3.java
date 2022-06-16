a0882d4f846ece64afc0349743ad340ab4ea1121
Rahat <105872616+Rahat-Kabir@users.noreply.github.com>
2022-06-16 10:54:28 -0700

    Add files via upload

---
+++ Lab3HT3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lab3HT3 {
+	
+	/* Find the Sum of digit of a Number
+	 EX. Sum of digits of 432 is 4+2+3 = 9 */
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.println("Enter a integer number : ");
+		num=input.nextInt();
+		int sum = 0;
+		while(num>0) {
+			int reminder = num%10;
+			sum+=reminder;
+			num=num/10;
+			
+		}
+		System.out.println("Sum of digits is : "+sum);
+	}
+
+}
