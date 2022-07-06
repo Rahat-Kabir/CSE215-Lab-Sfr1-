ff07e674489755d57b2e12605ceffa85deb6b7ec
Rahat <105872616+Rahat-Kabir@users.noreply.github.com>
2022-07-06 10:10:41 +0600

    Add files via upload

---
+++ Lab7HomeTask1/Fraction.java
@@ -0,0 +1,65 @@
+public class Fraction {
+
+	private int numerator;
+	private int denominator;
+
+	public Fraction(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+
+	public int getNumerator() {
+		return numerator;
+	}
+
+	public void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+
+	public int getDenominator() {
+		return denominator;
+	}
+
+	public void setDenominator(int denominator) {
+		this.denominator = denominator;
+	}
+
+	@Override
+	public String toString() {
+		return "" + numerator + "/" + denominator;
+	}
+
+	public void add(Fraction fraction) {
+
+		int num = this.getNumerator() * fraction.getDenominator() + this.getDenominator() * fraction.getNumerator();
+		int deno = this.getDenominator() * fraction.getDenominator();
+
+		this.setNumerator(num);
+		this.setDenominator(deno);
+	}
+
+	public void sub(Fraction fraction) {
+		int num = this.getNumerator() * fraction.getDenominator() - this.getDenominator() * fraction.getNumerator();
+		int deno = this.getDenominator() * fraction.getDenominator();
+
+		this.setNumerator(num);
+		this.setDenominator(deno);
+	}
+
+	public void multiplication(Fraction fraction) {
+		int num = this.getNumerator() * fraction.getNumerator();
+		int deno = this.getDenominator() * fraction.getDenominator();
+
+		this.setNumerator(num);
+		this.setDenominator(deno);
+	}
+
+	public void division(Fraction fraction) {
+
+		int num = this.getNumerator() * fraction.getDenominator();
+		int deno = this.getDenominator() * fraction.getNumerator();
+
+		this.setNumerator(num);
+		this.setDenominator(deno);
+	}
+}
